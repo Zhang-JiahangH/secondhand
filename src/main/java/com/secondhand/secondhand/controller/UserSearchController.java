ac49013684f40f9510e98ec26790c8dc5934fc6b
thomasxuprojectgit <xunannhj429@gmail.com>
2022-08-17 21:04:18 -0500

    Add function to get current user information after login

--- src/main/java/com/secondhand/secondhand/controller/UserSearchController.java
+++ src/main/java/com/secondhand/secondhand/controller/UserSearchController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 /**
  * Controller for user search
  */
@@ -22,5 +24,10 @@ public class UserSearchController {
     @GetMapping(value = "/user/search/{username}")
     public User searchUser(@PathVariable String username) {
         return userSearchService.search(username);
+    }
+
+    @GetMapping(value = "/user/getinfo")
+    public User getInfo(Principal principal) {
+        return userSearchService.search(principal.getName());
     }
 }
